4d99493d5ff372879da744b42db81ae18a8d3726
devrald24 <geraldmichaelaustria@gmail.com>
2016-12-22 16:30:39 +0800

    Code Reformat

--- ConnectJS/connect.js
+++ ConnectJS/connect.js
@@ -1,20 +1,65 @@
-var c = function(){
+(function(global, conn) {
+    "use strict";
 
-}
-c.connected = function(callback){
-  window.addEventListener('online',callback);
-}
-c.disconnected = function(callback){
-  window.addEventListener('offline',callback); 
-}
-c.statusChange = function(obj){
-  window.addEventListener('online',obj.onConnect);
-  window.addEventListener('offline',obj.onDisconnect);
-}
-c.isConnected = function(){
-  return navigator.onLine;
-}
-window.ConnectJS = c;
-if(!window.$$){
-  window.$$ = c;
-}
+    if (global.document) {
+        conn(global)
+    } else {
+        return;
+    }
+
+})(typeof window !== "undefined" ? window : this, function(window, noGlobal) {
+    var document = window.document;
+    var version = "1.0.0",
+        ConnectJS = function() {
+        	console.log("ConnectJS Initialized")
+        };
+
+    ConnectJS.fn = ConnectJS.prototype = {
+        connectjs: version
+    }
+
+    ConnectJS.extend = ConnectJS.fn.extend = function(){
+    		var name;
+    		var target = arguments[0];
+				ConnectJS = target;
+
+    		return ConnectJS;
+    }
+
+    ConnectJS.extend({
+    		connected:function(callback){
+    			window.addEventListener('online',callback);
+    		},
+    		disconnected:function(callback){
+    			window.addEventListener('offline',callback);
+    		},
+    		statusChange:function(obj){
+				  window.addEventListener('online',obj.onConnect);
+				  window.addEventListener('offline',obj.onDisconnect);
+				},
+    		isConnected:function(){
+    			return navigator.onLine;
+    		}
+    });
+
+    var _ConnectJS = window.ConnectJS,
+        _$$ = window.$$;
+
+    ConnectJS.noConflict = function() {
+        if (window.$$ === jQuery) {
+            window.$$ = _$$;
+        }
+
+        if (window.jQuery === jQuery) {
+            window.ConnectJS = _ConnectJS;
+        }
+
+        return ConnectJS;
+    };
+
+    if (!noGlobal) {
+        window.ConnectJS = window.$$ = ConnectJS;
+    }
+
+    return ConnectJS;
+});
